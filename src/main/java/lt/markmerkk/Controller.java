5a1b13a9211fdb0cbf1378adfd023fc638744129
Marius Merkevicius <mariusmerkevicius@Mariuss-MBP.zyxel.com>
2015-10-18 19:31:36 +0300

    Added necessary libraries to load project. Added gradle configuration. Empty project is ready for development.

--- src/main/java/lt/markmerkk/Controller.java
+++ src/main/java/lt/markmerkk/Controller.java
@@ -1,4 +1,4 @@
-package sample;
+package lt.markmerkk;
 
 public class Controller {
 }
--- src/main/java/lt/markmerkk/Main.java
+++ src/main/java/lt/markmerkk/Main.java
@@ -1,4 +1,4 @@
-package sample;
+package lt.markmerkk;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -10,7 +10,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/sample.fxml"));
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
