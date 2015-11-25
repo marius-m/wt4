ec3b4bdfa7078c5bdd782572255536bd8bf09a2c
Marius Merkevicius <marius.m@ito.lt>
2015-11-25 08:28:00 +0200

    Some ideas for jira syncing process.

--- app/src/main/java/lt/markmerkk/jira/JiraController.java
+++ app/src/main/java/lt/markmerkk/jira/JiraController.java
@@ -1,16 +1,15 @@
 package lt.markmerkk.jira;
 
-import com.atlassian.httpclient.api.HttpClient;
 import com.atlassian.jira.rest.client.api.JiraRestClient;
 import com.atlassian.jira.rest.client.api.RestClientException;
 import com.atlassian.jira.rest.client.api.domain.User;
-import com.atlassian.jira.rest.client.auth.AnonymousAuthenticationHandler;
-import com.atlassian.jira.rest.client.auth.BasicHttpAuthenticationHandler;
 import com.atlassian.jira.rest.client.internal.async.AsynchronousJiraRestClientFactory;
 import com.atlassian.util.concurrent.Promise;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import org.apache.commons.logging.Log;
 
 /**
@@ -21,39 +20,43 @@ public class JiraController implements IRemote {
 
   Log log;
   private JiraRestClient restClient;
+  private final ExecutorService executorService;
+  private Runnable userGetterRunnable;
 
   public JiraController(Log log) {
     this.log = log;
+    executorService = Executors.newSingleThreadExecutor();
   }
 
-  @Override public boolean isConnectionValid(String url, String username, String password) {
-    log.info("Testing connection to jira...");
-    try {
-      final AsynchronousJiraRestClientFactory factory = new AsynchronousJiraRestClientFactory();
-      restClient = factory.createWithBasicHttpAuthentication(
-          new URI(url), username, password);
-
-      Promise<User> promiseUser = restClient.getUserClient().getUser(username);
-      User user = promiseUser.claim();
-      log.info("Jira works! "+user.toString());
-      return true;
-    } catch (URISyntaxException e) {
-      log.info("Error connecting to jira! "+e.getMessage());
-      return false;
-    } catch (RestClientException e) {
-      log.info("Error connecting to jira! "+e.getMessage());
-      return false;
-    } catch (IllegalArgumentException e) {
-      log.info("Error connecting to jira! "+e.getMessage());
-      return false;
-    } finally {
-      close();
-    }
+  @Override public boolean isConnectionValid(final String url, final String username, final String password) {
+    userGetterRunnable = new Runnable() {
+      @Override public void run() {
+        log.info("Testing connection to jira...");
+        try {
+          final AsynchronousJiraRestClientFactory factory = new AsynchronousJiraRestClientFactory();
+          restClient = factory.createWithBasicHttpAuthentication(new URI(url), username, password);
+          Promise<User> promiseUser = restClient.getUserClient().getUser(username);
+          User user = promiseUser.claim();
+          log.info("Jira works! " + user.toString());
+        } catch (URISyntaxException e) {
+          log.info("Error connecting to jira! " + e.getMessage());
+        } catch (RestClientException e) {
+          log.info("Error connecting to jira! " + e.getMessage());
+        } catch (IllegalArgumentException e) {
+          log.info("Error connecting to jira! " + e.getMessage());
+        } finally {
+          close();
+        }
+      }
+    };
+    executorService.execute(userGetterRunnable);
+    return true;
   }
 
   @Override public void destroy() {
     log = null;
     close();
+    executorService.shutdown();
   }
 
   /**
