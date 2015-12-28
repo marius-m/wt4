57af0ac759806aa4e3dfc5a43ef4c9825adafb6f
Marius Merkevicius <marius.m@ito.lt>
2015-12-28 19:39:21 +0200

    Hooked settings class for saving, restoring instance for persistent storage.

--- app/src/main/java/lt/markmerkk/ui/settings/SettingsPresenter.java
+++ app/src/main/java/lt/markmerkk/ui/settings/SettingsPresenter.java
@@ -2,8 +2,15 @@ package lt.markmerkk.ui.settings;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javax.inject.Inject;
 import lt.markmerkk.listeners.Destroyable;
+import lt.markmerkk.utils.AdvHashSettings;
+import lt.markmerkk.utils.UserSettings;
 
 /**
  * Created by mariusmerkevicius on 12/20/15.
@@ -11,11 +18,26 @@ import lt.markmerkk.listeners.Destroyable;
  */
 public class SettingsPresenter implements Initializable, Destroyable {
 
+  @Inject
+  UserSettings settings;
+
+  @FXML
+  TextField inputHost, inputUsername;
+  @FXML
+  PasswordField inputPassword;
+  @FXML
+  TextArea outputLogger;
+
+
   @Override public void initialize(URL location, ResourceBundle resources) {
-    System.out.println("Hello world!");
+    inputHost.setText(settings.getHost());
+    inputUsername.setText(settings.getUsername());
+    inputPassword.setText(settings.getPassword());
   }
 
   @Override public void destroy() {
-    System.out.println("Bye!");
+    settings.setHost(inputHost.getText());
+    settings.setUsername(inputUsername.getText());
+    settings.setPassword(inputPassword.getText());
   }
 }
