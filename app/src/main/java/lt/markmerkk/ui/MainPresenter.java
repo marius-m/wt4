7172d319095f0f86566829bc4113819e03a551af
Marius Merkevicius <marius.m@ito.lt>
2016-01-06 00:14:39 +0200

    Wrapped tabs to separate controller.

--- app/src/main/java/lt/markmerkk/ui/MainPresenter.java
+++ app/src/main/java/lt/markmerkk/ui/MainPresenter.java
@@ -2,8 +2,6 @@ package lt.markmerkk.ui;
 
 import java.net.URL;
 import java.util.ResourceBundle;
-import javafx.application.Platform;
-import javafx.collections.ListChangeListener;
 import javafx.event.Event;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
@@ -11,18 +9,17 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.StackPane;
 import lt.markmerkk.listeners.Destroyable;
 import lt.markmerkk.storage2.SimpleLog;
 import lt.markmerkk.ui.clock.ClockPresenter;
 import lt.markmerkk.ui.clock.ClockView;
 import lt.markmerkk.ui.display.DisplayLogPresenter;
 import lt.markmerkk.ui.display.DisplayLogView;
-import lt.markmerkk.ui.settings.SettingsPresenter;
 import lt.markmerkk.ui.settings.SettingsView;
 import lt.markmerkk.ui.status.StatusView;
 import lt.markmerkk.ui.update.UpdateLogPresenter;
 import lt.markmerkk.ui.update.UpdateLogView;
+import lt.markmerkk.utils.HiddenTabsController;
 
 /**
  * Created by mariusmerkevicius on 12/5/15.
@@ -34,6 +31,12 @@ public class MainPresenter implements Initializable {
   @FXML BorderPane northPane;
   @FXML BorderPane southPane;
 
+  HiddenTabsController tabsController;
+
+  public MainPresenter() {
+    tabsController = new HiddenTabsController();
+  }
+
   @Override public void initialize(URL location, ResourceBundle resources) {
     ClockView clockView = new ClockView(clockListener);
     northPane.setCenter(clockView.getView());
@@ -41,7 +44,7 @@ public class MainPresenter implements Initializable {
     southPane.setBottom(statusView.getView());
 
     displayLogs();
-    modifyTabShowing();
+    tabsController.prepare(tabPane);
   }
 
   //region Convenience
@@ -62,43 +65,13 @@ public class MainPresenter implements Initializable {
     southPane.setCenter(updateLogView.getView());
   }
 
-  /**
-   * Modifies tab showing depending on displayed tabs.
-   * Uses a dirty hack to display tabs only when there are more than one tag.
-   * Source: https://gist.github.com/twasyl/7fc08b5843964823e36b
-   */
-  private void modifyTabShowing() {
-    Platform.runLater(() -> {
-      tabPane.getTabs().addListener((ListChangeListener) change -> {
-        final StackPane header = (StackPane) tabPane.lookup(".tab-header-area");
-        if (header != null) {
-          if (this.tabPane.getTabs().size() == 1) header.setPrefHeight(0);
-          else header.setPrefHeight(-1);
-        }
-      });
-      Tab mockTab = new Tab();
-      tabPane.getTabs().add(mockTab);
-      tabPane.getTabs().remove(mockTab);
-    });
-  }
-
   //endregion
 
   //region Listeners
 
   ClockPresenter.Listener clockListener = new ClockPresenter.Listener() {
     @Override public void onNew() {
-      Tab mockTab = new Tab("Settings");
-      final SettingsView settingsView = new SettingsView();
-      mockTab.setContent(settingsView.getView());
-      tabPane.getTabs().add(mockTab);
-      tabPane.getSelectionModel().select(mockTab);
-      mockTab.setOnClosed(new EventHandler<Event>() {
-        @Override public void handle(Event event) {
-          if (settingsView.getPresenter() instanceof Destroyable)
-            ((Destroyable) settingsView.getPresenter()).destroy();
-        }
-      });
+      tabsController.addCloseableTab(new SettingsView(), "Settings");
     }
   };
 
