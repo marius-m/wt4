1224f688978e8f8cb512c8bce5c3496a80a38f9a
Marius Merkevicius <marius.m@ito.lt>
2016-01-09 14:29:45 +0200

    Resetting keep alive to be one instance.

--- app/src/main/java/lt/markmerkk/utils/hourglass/KeepAliveController.java
+++ app/src/main/java/lt/markmerkk/utils/hourglass/KeepAliveController.java
@@ -20,6 +20,7 @@ public class KeepAliveController {
   Listener listener;
   long delay = MINUTE;
 
+  @PostConstruct
   public void start() {
     TimerTask updateRunnable = new TimerTask() {
       @Override public void run() {
@@ -31,6 +32,7 @@ public class KeepAliveController {
     timer.scheduleAtFixedRate(updateRunnable, 1, delay);
   }
 
+  @PreDestroy
   public void stop() {
     if (timer == null) return;
     timer.cancel();
