002b7202e90abcb207acb97eb80477f72771b92f
Marius Merkevicius <marius.m@ito.lt>
2016-01-23 12:26:08 +0200

    Commenting out the update code part in the main class.

--- app/src/main/java/lt/markmerkk/Main.java
+++ app/src/main/java/lt/markmerkk/Main.java
@@ -41,90 +41,90 @@ public class Main extends Application {
 
     public Main() { }
 
-    private ProgressIndicator showGiantProgressWheel(Stage stage) {
-        ProgressIndicator indicator = new ProgressIndicator();
-        BorderPane borderPane = new BorderPane(indicator);
-        borderPane.setMinWidth(640);
-        borderPane.setMinHeight(480);
-        Button pinButton = new Button();
-        pinButton.setText("Pin to version 1");
-        pinButton.setOnAction(event -> {
-            UpdateFX.pinToVersion(AppDirectory.dir(), 1);
-            UpdateFX.restartApp();
-        });
-        HBox box = new HBox(new Label("Version " + VERSION), pinButton);
-        box.setSpacing(10);
-        box.setAlignment(Pos.CENTER_LEFT);
-        box.setPadding(new Insets(10));
-        borderPane.setTop(box);
-        Scene scene = new Scene(borderPane);
-        stage.setScene(scene);
-        return indicator;
-    }
+//    private ProgressIndicator showGiantProgressWheel(Stage stage) {
+//        ProgressIndicator indicator = new ProgressIndicator();
+//        BorderPane borderPane = new BorderPane(indicator);
+//        borderPane.setMinWidth(640);
+//        borderPane.setMinHeight(480);
+//        Button pinButton = new Button();
+//        pinButton.setText("Pin to version 1");
+//        pinButton.setOnAction(event -> {
+//            UpdateFX.pinToVersion(AppDirectory.dir(), 1);
+//            UpdateFX.restartApp();
+//        });
+//        HBox box = new HBox(new Label("Version " + VERSION), pinButton);
+//        box.setSpacing(10);
+//        box.setAlignment(Pos.CENTER_LEFT);
+//        box.setPadding(new Insets(10));
+//        borderPane.setTop(box);
+//        Scene scene = new Scene(borderPane);
+//        stage.setScene(scene);
+//        return indicator;
+//    }
 
     @Override
     public void start(Stage stage) throws Exception{
-        // For some reason the JavaFX launch process results in us losing the thread context class loader: reset it.
-        Thread.currentThread().setContextClassLoader(Main.class.getClassLoader());
-        // Must be done twice for the times when we come here via realMain.
-        AppDirectory.initAppDir("UpdateFX Example App");
-
-        log.info("Hello World! This is version " + VERSION);
-
-        ProgressIndicator indicator = showGiantProgressWheel(stage);
-
-        List<ECPoint> pubkeys = Crypto.decode(
-            "020044E154894596A94EF649DC203358C27A310DDBE4D22646AC56BC8FB0BFBAFB",
-            "0292BAC95D764ADBF15367ABE490708753FE2E92760F7C9FF6951EE558F57AE407");
-        Updater updater = new Updater(URI.create("http://localhost:8000/files"), "main/" + VERSION,
-            AppDirectory.dir(), UpdateFX.findCodePath(Main.class), pubkeys, 1) {
-            @Override
-            protected void updateProgress(long workDone, long max) {
-                super.updateProgress(workDone, max);
-                // Give UI a chance to show.
-                Uninterruptibles.sleepUninterruptibly(100, TimeUnit.MILLISECONDS);
-            }
-        };
-
-        indicator.progressProperty().bind(updater.progressProperty());
-
-        log.info("Checking for updates!");
-        updater.setOnSucceeded(event -> {
-            try {
-                UpdateSummary summary = updater.get();
-                if (summary.descriptions.size() > 0) {
-                    log.info("One liner: {}", summary.descriptions.get(0).getOneLiner());
-                    log.info("{}", summary.descriptions.get(0).getDescription());
-                }
-                if (summary.highestVersion > VERSION) {
-                    log.info("Restarting to get version " + summary.highestVersion);
-                    if (UpdateFX.getVersionPin(AppDirectory.dir()) == 0)
-                        UpdateFX.restartApp();
-                }
-            } catch (Throwable e) {
-                log.error("oops", e);
-            }
-        });
-        updater.setOnFailed(event -> {
-            log.error("Update error: {}", updater.getException());
-            updater.getException().printStackTrace();
-        });
-
-        indicator.setOnMouseClicked(ev -> UpdateFX.restartApp());
-
-        new Thread(updater, "UpdateFX Thread").start();
-        stage.show();
-
-//        MainView mainView = new MainView(stage);
-//        Scene scene = new Scene(mainView.getView());
-//        scene.getStylesheets().add(
-//            getClass().getResource("/text-field-red-border.css").toExternalForm());
-//        stage.setWidth(SCENE_WIDTH);
-//        stage.setHeight(SCENE_HEIGHT);
-//        stage.setMinWidth(SCENE_WIDTH);
-//        stage.setMinHeight(SCENE_HEIGHT);
-//        stage.setScene(scene);
+//        // For some reason the JavaFX launch process results in us losing the thread context class loader: reset it.
+//        Thread.currentThread().setContextClassLoader(Main.class.getClassLoader());
+//        // Must be done twice for the times when we come here via realMain.
+//        AppDirectory.initAppDir("UpdateFX Example App");
+//
+//        log.info("Hello World! This is version " + VERSION);
+//
+//        ProgressIndicator indicator = showGiantProgressWheel(stage);
+//
+//        List<ECPoint> pubkeys = Crypto.decode(
+//            "020044E154894596A94EF649DC203358C27A310DDBE4D22646AC56BC8FB0BFBAFB",
+//            "0292BAC95D764ADBF15367ABE490708753FE2E92760F7C9FF6951EE558F57AE407");
+//        Updater updater = new Updater(URI.create("http://localhost:8000/files"), "main/" + VERSION,
+//            AppDirectory.dir(), UpdateFX.findCodePath(Main.class), pubkeys, 1) {
+//            @Override
+//            protected void updateProgress(long workDone, long max) {
+//                super.updateProgress(workDone, max);
+//                // Give UI a chance to show.
+//                Uninterruptibles.sleepUninterruptibly(100, TimeUnit.MILLISECONDS);
+//            }
+//        };
+//
+//        indicator.progressProperty().bind(updater.progressProperty());
+//
+//        log.info("Checking for updates!");
+//        updater.setOnSucceeded(event -> {
+//            try {
+//                UpdateSummary summary = updater.get();
+//                if (summary.descriptions.size() > 0) {
+//                    log.info("One liner: {}", summary.descriptions.get(0).getOneLiner());
+//                    log.info("{}", summary.descriptions.get(0).getDescription());
+//                }
+//                if (summary.highestVersion > VERSION) {
+//                    log.info("Restarting to get version " + summary.highestVersion);
+//                    if (UpdateFX.getVersionPin(AppDirectory.dir()) == 0)
+//                        UpdateFX.restartApp();
+//                }
+//            } catch (Throwable e) {
+//                log.error("oops", e);
+//            }
+//        });
+//        updater.setOnFailed(event -> {
+//            log.error("Update error: {}", updater.getException());
+//            updater.getException().printStackTrace();
+//        });
+//
+//        indicator.setOnMouseClicked(ev -> UpdateFX.restartApp());
+//
+//        new Thread(updater, "UpdateFX Thread").start();
 //        stage.show();
+
+        MainView mainView = new MainView(stage);
+        Scene scene = new Scene(mainView.getView());
+        scene.getStylesheets().add(
+            getClass().getResource("/text-field-red-border.css").toExternalForm());
+        stage.setWidth(SCENE_WIDTH);
+        stage.setHeight(SCENE_HEIGHT);
+        stage.setMinWidth(SCENE_WIDTH);
+        stage.setMinHeight(SCENE_HEIGHT);
+        stage.setScene(scene);
+        stage.show();
     }
 
     @Override public void stop() throws Exception {
@@ -133,12 +133,13 @@ public class Main extends Application {
     }
 
     public static void main(String[] args) throws IOException {
-        AppDirectory.initAppDir("UpdateFX Example App");
-        UpdateFX.bootstrap(Main.class, AppDirectory.dir(), args);
-    }
-
-    public static void realMain(String[] args) {
+//        AppDirectory.initAppDir("UpdateFX Example App");
+//        UpdateFX.bootstrap(Main.class, AppDirectory.dir(), args);
         launch(args);
     }
+
+//    public static void realMain(String[] args) {
+//        launch(args);
+//    }
 
 }
