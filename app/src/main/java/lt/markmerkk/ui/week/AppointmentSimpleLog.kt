9f8362e3581a0569eafd87bf58e0413ef2bf17cb
Marius Merkevičius <merkevicius.marius@gmail.com>
2016-07-09 18:45:12 +0300

    Kotlin conversions

---
+++ app/src/main/java/lt/markmerkk/ui/week/AppointmentSimpleLog.kt
@@ -0,0 +1,12 @@
+package lt.markmerkk.ui.week
+
+import jfxtras.scene.control.agenda.Agenda
+import jfxtras.scene.control.agenda.Agenda.AppointmentImplLocal
+import lt.markmerkk.storage2.SimpleLog
+
+/**
+ * Created by mariusmerkevicius on 1/22/16.
+ * Represents an appointment for the [Agenda] that would represent a simple
+ * log
+ */
+class AppointmentSimpleLog(val simpleLog: SimpleLog) : AppointmentImplLocal() {}
