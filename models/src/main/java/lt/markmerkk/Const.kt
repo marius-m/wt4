d3b4c2e2d08fd45950ae61b8a2bebbedc6c4d573
Marius Merkevicius <mariusmerkevicius@Mariuss-MacBook-Pro-2.local>
2016-08-20 14:39:55 +0300

    Correct for default issue search

--- models/src/main/java/lt/markmerkk/Const.kt
+++ models/src/main/java/lt/markmerkk/Const.kt
@@ -5,13 +5,7 @@ package lt.markmerkk
  * @since 2016-08-09
  */
 object Const {
-    val DEFAULT_JQL_WORKLOG_TEMPLATE = "key in workedIssues(\"%s\", \"%s\", \"%s\")"
-    val DEFAULT_JQL_USER_ISSUES = "(status not in (closed, resolved)) AND (assignee = currentUser() OR reporter = currentUser())"
-
-//    var DEBUG = true
-
-//    val cfgHome: String by lazy {
-//
-//    }
-
+    val DEFAULT_JQL_USER_ISSUES = "(status not in (closed, resolved, done))" +
+            " AND " +
+            "(assignee = currentUser() OR reporter = currentUser() OR watcher = currentUser())"
 }
