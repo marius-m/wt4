b536c14b5b5150776d89fe511cb2b345dd05e1d7
Marius Merkevicius <merkevicius.marius@gmail.com>
2016-11-22 21:51:17 +0200

    Finished integration testing for query generation

--- components/src/test/java/lt/markmerkk/interactors/SearchQueryGeneratorImplIntegrationTest.kt
+++ components/src/test/java/lt/markmerkk/interactors/SearchQueryGeneratorImplIntegrationTest.kt
@@ -3,6 +3,7 @@ package lt.markmerkk.interactors
 import lt.markmerkk.LikeQueryGeneratorImpl
 import lt.markmerkk.utils.IssueSplitImpl
 import org.junit.Assert.*
+import org.junit.Ignore
 import org.junit.Test
 
 /**
@@ -37,6 +38,35 @@ class SearchQueryGeneratorImplIntegrationTest {
         val result = generator.searchQuery("input1")
 
         // Assert
-        assertEquals("(description like '%%input1%%') OR (key like '%%INPUT-1%%')", result)
+        assertEquals("((description like '%%input1%%') OR (key like '%%INPUT-1%%'))", result)
+    }
+
+    @Test
+    fun valid2_returnQuery() {
+        // Arrange
+        // Act
+        val result = generator.searchQuery("test")
+
+        // Assert
+        assertEquals("((description like '%%test%%'))", result)
+    }
+
+    @Test
+    fun valid3_returnQuery() {
+        // Arrange
+        // Act
+        val result = generator.searchQuery("test test2 test3 test4")
+
+        // Assert
+        assertEquals("(" +
+                "(" +
+                "description like '%%test%%' " +
+                "OR description like '%%test2%%' " +
+                "OR description like '%%test3%%' " +
+                "OR description like '%%test4%%'" +
+                ") " +
+                "OR " +
+                "(key like '%%TEST-2%%')" +
+                ")", result)
     }
 }
