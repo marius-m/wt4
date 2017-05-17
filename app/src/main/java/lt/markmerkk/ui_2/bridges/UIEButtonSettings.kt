de8a4b35cef99e668848e0455d81a7d167a3c5f7
Marius Merkevicius <marius.merkevicius@wahanda.com>
2017-05-17 23:48:12 +0300

    Update icons for context menu

--- app/src/main/java/lt/markmerkk/ui_2/bridges/UIEButtonSettings.kt
+++ app/src/main/java/lt/markmerkk/ui_2/bridges/UIEButtonSettings.kt
@@ -24,11 +24,14 @@ class UIEButtonSettings(
 ) : UIElement<JFXButton> {
 
     private val glyphSettings: SVGGlyph = settingsGlyph(Color.WHITE, 20.0)
+    private val glyphStatistics: SVGGlyph = statisticsGlyph(Color.BLACK, 20.0)
+    private val glyphPaint: SVGGlyph = paintGlyph(Color.BLACK, 20.0)
+    private val glyphRefresh: SVGGlyph = refreshGlyph(Color.BLACK, 20.0, 16.0)
     private var jfxPopup: JFXPopup = JFXPopup()
 
-    private val labelStatistics = Label("Statistics", settingsGlyph(Color.BLACK, 12.0))
-    private val labelRefresh = Label("Force refresh", settingsGlyph(Color.BLACK, 12.0))
-    private val labelBackToDefault = Label("Default view", settingsGlyph(Color.BLACK, 12.0))
+    private val labelStatistics = Label("Statistics", glyphStatistics)
+    private val labelRefresh = Label("Force refresh", glyphRefresh)
+    private val labelBackToDefault = Label("Default view", glyphPaint)
 
     init {
         button.graphic = glyphSettings
@@ -76,7 +79,7 @@ class UIEButtonSettings(
     private fun handleSelection(selectLabel: Label) {
         when (selectLabel) {
             labelStatistics -> println("Show statistics")
-            labelRefresh -> println("Show statistics")
+            labelRefresh -> println("Forcing a refresh")
             labelBackToDefault -> {
                 Main.Companion.MATERIAL = false
                 Main.Companion.mainInstance!!.restart()
@@ -100,6 +103,40 @@ class UIEButtonSettings(
         svgGlyph.setSize(size, size)
         return svgGlyph
     }
+
+    private fun statisticsGlyph(color: Color, size: Double): SVGGlyph {
+        val svgGlyph = SVGGlyph(
+                -1,
+                "statistics",
+                "M10 20h4V4h-4v16zm-6 0h4v-8H4v8zM16 9v11h4V9h-4z",
+                color
+        )
+        svgGlyph.setSize(size, size)
+        return svgGlyph
+    }
+
+    private fun refreshGlyph(color: Color, width: Double, height: Double): SVGGlyph {
+        val svgGlyph = SVGGlyph(
+                -1,
+                "refresh",
+                "M19 8l-4 4h3c0 3.31-2.69 6-6 6-1.01 0-1.97-.25-2.8-.7l-1.46 1.46C8.97 19.54 10.43 20 12 20c4.42 0 8-3.58 8-8h3l-4-4zM6 12c0-3.31 2.69-6 6-6 1.01 0 1.97.25 2.8.7l1.46-1.46C15.03 4.46 13.57 4 12 4c-4.42 0-8 3.58-8 8H1l4 4 4-4H6z",
+                color
+        )
+        svgGlyph.setSize(width, height)
+        return svgGlyph
+    }
+
+    private fun paintGlyph(color: Color, size: Double): SVGGlyph {
+        val svgGlyph = SVGGlyph(
+                -1,
+                "paint",
+                "M7 14c-1.66 0-3 1.34-3 3 0 1.31-1.16 2-2 2 .92 1.22 2.49 2 4 2 2.21 0 4-1.79 4-4 0-1.66-1.34-3-3-3zm13.71-9.37l-1.34-1.34c-.39-.39-1.02-.39-1.41 0L9 12.25 11.75 15l8.96-8.96c.39-.39.39-1.02 0-1.41z",
+                color
+        )
+        svgGlyph.setSize(size, size)
+        return svgGlyph
+    }
+
 
     //endregion
 
