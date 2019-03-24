1fea78db90318e6ed318730ec6c8d8743a6058ff
Marius Merkevicius <merkevicius.marius@gmail.com>
2019-03-24 23:22:42 +0200

    Change generated source target

--- database2/generate/generate.sh
+++ database2/generate/generate.sh
@@ -2,6 +2,6 @@
 DB_NAME=wt4_2.db
 set -x
 rm $DB_NAME
-rm -r build
+rm -r src
 sqlite3 $DB_NAME < initial.sql
 java -cp jooq-3.11.10.jar:jooq-meta-3.11.10.jar:jooq-codegen-3.11.10.jar:sqlite-jdbc-3.25.2.jar:. org.jooq.codegen.GenerationTool jooq.xml
