01fce7c038b3691400178c92d5840e2019dbe009
Marius Merkevicius <merkevicius.marius@gmail.com>
2020-06-15 23:14:23 +0300

    Enforce file paths

--- app/src/main/java/lt/markmerkk/MainAsJava.java
+++ app/src/main/java/lt/markmerkk/MainAsJava.java
@@ -3,6 +3,12 @@ package lt.markmerkk;
 import static javafx.application.Application.launch;
 
 public class MainAsJava {
+
+    static {
+        System.setProperty("file.encoding", "UTF-8");
+        System.setProperty("sun.jnu.encoding", "UTF-8");
+    }
+
     public static void main(String[] args) {
         launch(Main.class);
     }
