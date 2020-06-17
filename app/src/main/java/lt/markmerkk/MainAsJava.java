e446898180c8675f562345a480a54e062a50e5cc
Marius Merkevicius <merkevicius.marius@gmail.com>
2020-06-17 23:21:57 +0300

    Encoding

--- app/src/main/java/lt/markmerkk/MainAsJava.java
+++ app/src/main/java/lt/markmerkk/MainAsJava.java
@@ -1,5 +1,10 @@
 package lt.markmerkk;
 
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+
 import static javafx.application.Application.launch;
 
 public class MainAsJava {
@@ -15,8 +20,24 @@ public class MainAsJava {
     }
 
     public static void main(String[] args) {
-        System.setProperty("file.encoding", "UTF-8");
-        System.setProperty("sun.jnu.encoding", "UTF-8");
+        String defaultCharacterEncoding = System.getProperty("file.encoding");
+        System.out.println("defaultCharacterEncoding by property: " + defaultCharacterEncoding);
+        System.out.println("defaultCharacterEncoding by code: " + getDefaultCharEncoding());
+        System.out.println("defaultCharacterEncoding by charSet: " + Charset.defaultCharset());
+
+        System.setProperty("file.encoding", "UTF-16");
+        System.out.println("defaultCharacterEncoding by property after updating file.encoding : " + System.getProperty("file.encoding"));
+        System.out.println("defaultCharacterEncoding by code after updating file.encoding : " + getDefaultCharEncoding());
+        System.out.println("defaultCharacterEncoding by java.nio.Charset after updating file.encoding : " + Charset.defaultCharset());
+
         launch(Main.class);
+    }
+
+    public static String getDefaultCharEncoding(){
+        byte [] bArray = {'w'};
+        InputStream is = new ByteArrayInputStream(bArray);
+        InputStreamReader reader = new InputStreamReader(is);
+        final String defaultCharacterEncoding = reader.getEncoding();
+        return defaultCharacterEncoding;
     }
 }
