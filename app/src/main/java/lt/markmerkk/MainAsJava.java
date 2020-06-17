356420a137e45ce3e953ddcdfab8e2e8fbbee7c1
Marius Merkevicius <merkevicius.marius@gmail.com>
2020-06-17 22:55:10 +0300

    Change byte encoding

--- app/src/main/java/lt/markmerkk/MainAsJava.java
+++ app/src/main/java/lt/markmerkk/MainAsJava.java
@@ -1,5 +1,8 @@
 package lt.markmerkk;
 
+import java.nio.ByteBuffer;
+import java.nio.charset.StandardCharsets;
+
 import static javafx.application.Application.launch;
 
 public class MainAsJava {
@@ -9,8 +12,9 @@ public class MainAsJava {
         System.setProperty("sun.jnu.encoding", "UTF-8");
         final String tmpPath = System.getProperty("java.io.tmpdir");
         System.out.println("Tmp path: " + tmpPath);
-        System.setProperty("java.io.tmpdir", tmpPath);
-        System.out.println("Rebind tmp path" + System.getProperty("java.io.tmpdir"));
+        final ByteBuffer byteBuffer = StandardCharsets.UTF_8.encode(tmpPath);
+        System.setProperty("java.io.tmpdir", byteBuffer.toString());
+        System.out.println("Rebind tmp path: " + System.getProperty("java.io.tmpdir"));
     }
 
     public static void main(String[] args) {
