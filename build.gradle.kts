df2a700a47ab37e133dc5a9a3fc34e05a0253a27
Marius Merkevicius <merkevicius.marius@gmail.com>
2020-06-29 20:39:08 +0300

    Clean-up

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,47 +1,4 @@
 buildscript {
-//    apply from: "versions.gradle"
-//    ext.kotlin_test_version = '1.4.0'
-
-
-    // Version
-//    ext.versionName = "1.7.8"
-//    ext.versionCode = "63"
-
-    //region Debug basic properties
-    //endregion
-
-    //region Release oauth properties
-//    def keysPropertyFile = new File("${rootDir}/keys", "private.properties")
-//    assert keysPropertyFile.exists()
-//    Properties keysProperties = new Properties()
-//    keysProperties.load(new FileInputStream(keysPropertyFile.getAbsolutePath()))
-//
-//    def deployPropertyFile = new File("${rootDir}","deploy.properties")
-//    assert deployPropertyFile.exists()
-//    Properties deployProps = new Properties()
-//    deployProps.load(new FileInputStream(deployPropertyFile.getAbsolutePath()))
-//    ext.debug = false
-//    ext.gaKey = deployProps.getProperty("ga")
-//    ext.oauth = true
-//    ext.oauth_key_consumer = keysProperties.getProperty("key_consumer")
-//    ext.oauth_key_private = keysProperties.getProperty("key_private")
-//    ext.oauth_host = keysProperties.getProperty("host")
-    //endregion
-
-    //region Release basic properties
-//    def deployPropertyFile = new File("${rootDir}","deploy.properties")
-//    assert deployPropertyFile.exists()
-//    Properties deployProps = new Properties()
-//    deployProps.load(new FileInputStream(deployPropertyFile.getAbsolutePath()))
-//
-//    ext.debug = false
-//    ext.gaKey = deployProps.getProperty("ga")
-//    ext.oauth = false
-//    ext.oauth_key_consumer = ""
-//    ext.oauth_key_private = ""
-//    ext.oauth_host = ""
-    //endregion
-
     repositories {
         mavenCentral()
         jcenter()
@@ -61,17 +18,6 @@ buildscript {
 }
 
 allprojects {
-    ext {
-        set("versionName", "1.7.8")
-        set("versionCode", "63")
-        set("debug", true)
-        set("gaKey", "test")
-        set("oauth", false)
-        set("oauthKeyConsumer", "")
-        set("oauthKeyPrivate", "")
-        set("oauthHost", "")
-    }
-
     repositories {
         mavenCentral()
         jcenter()
