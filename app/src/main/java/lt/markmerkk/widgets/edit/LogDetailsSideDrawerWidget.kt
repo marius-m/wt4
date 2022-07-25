5d94a7cf46a75ea1253b131d0c28b4651966e9e2
Marius Merkevicius <marius.m@ito.lt>
2022-07-25 22:46:22 +0300

    Refocus selected value when showing selector

--- app/src/main/java/lt/markmerkk/widgets/edit/LogDetailsSideDrawerWidget.kt
+++ app/src/main/java/lt/markmerkk/widgets/edit/LogDetailsSideDrawerWidget.kt
@@ -272,6 +272,10 @@ class LogDetailsSideDrawerWidget : Fragment(),
                             val timePicker = this
                             setOnMouseClicked {
                                 viewTimePickerFromPopOver.show(timePicker)
+                                focusTimeSelectionInList(
+                                    timeAsRawInput = timePicker.text,
+                                    viewListViewTimePicker = viewTimePickerFromOptions,
+                                )
                             }
                         }
                         label("-")
@@ -315,6 +319,10 @@ class LogDetailsSideDrawerWidget : Fragment(),
                             val timePicker = this
                             setOnMouseClicked {
                                 viewTimePickerToPopOver.show(timePicker)
+                                focusTimeSelectionInList(
+                                    timeAsRawInput = timePicker.text,
+                                    viewListViewTimePicker = viewTimePickerToOptions,
+                                )
                             }
                         }
                     }
@@ -586,6 +594,8 @@ class LogDetailsSideDrawerWidget : Fragment(),
         viewTableRecent.focusedProperty().addListener(listenerFocusChangeListRecentTickets)
         viewTextFieldTicket.textProperty().addListener(listenerTextChangeInputTicket)
         viewTextFieldTicket.focusedProperty().addListener(listenerFocusChangeInputTicket)
+        viewTimePickerFrom.focusedProperty().addListener(listenerFocusChangeInputSelectTimeFrom)
+        viewTimePickerTo.focusedProperty().addListener(listenerFocusChangeInputSelectTimeTo)
         initTimePickSelectValues()
         root.setOnMouseClicked {
             popOverElementsCloseAll()
@@ -599,6 +609,8 @@ class LogDetailsSideDrawerWidget : Fragment(),
     }
 
     override fun onUndock() {
+        viewTimePickerTo.focusedProperty().removeListener(listenerFocusChangeInputSelectTimeTo)
+        viewTimePickerFrom.focusedProperty().removeListener(listenerFocusChangeInputSelectTimeFrom)
         viewTableRecent.focusedProperty().removeListener(listenerFocusChangeListRecentTickets)
         viewTextFieldTicket.textProperty().removeListener(listenerTextChangeInputTicket)
         viewTextFieldTicket.focusedProperty().removeListener(listenerFocusChangeInputTicket)
@@ -682,6 +694,18 @@ class LogDetailsSideDrawerWidget : Fragment(),
 
     private val listenerFocusChangeInputTicket = ChangeListener<Boolean> { _, _, newValue ->
         handleRecentVisibility()
+    }
+
+    private val listenerFocusChangeInputSelectTimeFrom = ChangeListener<Boolean> { _, _, newValue ->
+        if (!newValue) {
+            viewTimePickerFromPopOver.hide(Duration.ZERO)
+        }
+    }
+
+    private val listenerFocusChangeInputSelectTimeTo = ChangeListener<Boolean> { _, _, newValue ->
+        if (!newValue) {
+            viewTimePickerToPopOver.hide(Duration.ZERO)
+        }
     }
 
     private val listenerTimePickFrom = ChangeListener<BasicTimePickViewModel> { _, _, newValue ->
@@ -844,6 +868,19 @@ class LogDetailsSideDrawerWidget : Fragment(),
     fun focusInput() {
         viewTextComment.textArea.requestFocus()
         viewTextComment.textArea.positionCaret(viewTextComment.textArea.text.length)
+    }
+
+    private fun focusTimeSelectionInList(
+        timeAsRawInput: String,
+        viewListViewTimePicker: ListView<BasicTimePickViewModel>,
+    ) {
+        val selectValue = viewListViewTimePicker.items
+            .firstOrNull { it.timeAsString == timeAsRawInput }
+        if (selectValue != null) {
+            val selectValueIndex = viewListViewTimePicker.items.indexOf(selectValue)
+            viewListViewTimePicker.selectionModel.clearAndSelect(selectValueIndex)
+            viewListViewTimePicker.scrollTo(selectValueIndex)
+        }
     }
 
     private fun isPopOverElementsOpen(): Boolean {
